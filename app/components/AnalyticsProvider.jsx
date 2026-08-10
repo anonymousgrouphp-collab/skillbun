02cff2b4493087fc213f3c759c7254a850efac03
Team SkillBun <anonymousgrouphp@gmail.com>
2026-08-10 13:51:49 +0530

    feat(analytics): initialize PostHog via official posthog-js SDK v2.7.17

--- app/components/AnalyticsProvider.jsx
+++ app/components/AnalyticsProvider.jsx
@@ -3,6 +3,7 @@
 import { useEffect, Suspense } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import Script from 'next/script';
+import posthog from 'posthog-js';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { trackPageView, identifyUser } from '@/lib/analytics';
@@ -36,9 +37,18 @@ function AnalyticsTracker() {
 }
 
 export function AnalyticsProvider({ children }) {
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-XTFMS5Q59C';
-  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY || 'phc_vi8BxTy7vMdhuQn7WSmcQ4n4C2hPqBemqXEGTStm83HJ';
-  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com';
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const token = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN || process.env.NEXT_PUBLIC_POSTHOG_KEY || 'phc_vi8BxTy7vMdhuQn7WSmcQ4n4C2hPqBemqXEGTStm83HJ';
+      const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com';
+
+      posthog.init(token, {
+        api_host: host,
+        person_profiles: 'identified_only',
+        capture_pageview: true,
+      });
+    }
+  }, []);
 
   return (
     <>
@@ -52,15 +62,7 @@ export function AnalyticsProvider({ children }) {
       {/* Official Vercel Speed Insights Component */}
       <SpeedInsights />
 
-      {/* PostHog Product Analytics Snippet */}
-      <Script id="posthog-analytics" strategy="afterInteractive">
-        {`
-          !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group identify_group".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1.0)}(document,window.posthog||[]);
-          window.posthog.init('${posthogKey}', { api_host: '${posthogHost}', autocapture: true, capture_pageview: true });
-        `}
-      </Script>
-
-      {/* 3. Vercel Speed Insights & Performance Listener */}
+      {/* Vercel Speed Insights & Performance Listener */}
       <Script id="vercel-speed-insights" strategy="afterInteractive">
         {`
           (function(){
